b14c0df75b70bfe7067937e7864c5b3f542c2de4
Yaraslau Sedach <jaroslaw.sedach@gmail.com>
2025-03-07 14:35:41 +0100

    wip

--- frontend/src/components/ChatCard.jsx
+++ frontend/src/components/ChatCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ChatCard = ({message}) => {
   return (
-    <div className='flex items-center p-2 hover:bg-[hsl(0,0%,15%)] rounded-md cursor-pointer'>
+    <div className='flex items-center p-2 hover:bg-[hsl(0,0%,15%)] rounded-md cursor-pointer my-2'>
       <p className="text-white text-sm truncate">{message}</p>
     </div>
   )
