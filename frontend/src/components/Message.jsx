4ac72653165a38e02e4443e37852085df10bac6d
OGrocketa <jaroslaw.sedach@gmail.com>
2025-03-13 16:55:16 +0100

    feat: prettier response

--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
 
 export const Message = ({ message, timestamp, isOutgoing = false }) => {
   return (
     <div className={`flex mb-4 ${isOutgoing ? 'justify-end' : 'justify-start'}`}>
       <div className="max-w-[70%] m-1">
-        <div className={`p-3 rounded-lg shadow mx-2 ${
-          isOutgoing 
-            ? 'bg-green-400' 
-            : 'bg-gray-100'
-        }`}>
-          <p className="text-sm break-words whitespace-normal text-left">{message}</p>
+        <div className={`p-3 rounded-lg shadow mx-2 ${isOutgoing ? 'bg-green-400' : 'bg-gray-100'}`}>
+        <div className="text-sm break-words whitespace-normal text-left">
+          <ReactMarkdown>{message}</ReactMarkdown>
+        </div>
+
           {timestamp && (
-            <div className={`text-xs mt-1 text-right text-black`}>
+            <div className="text-xs mt-1 text-right text-black">
               {timestamp}
             </div>
           )}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
