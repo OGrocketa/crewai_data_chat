76b971f55c5c821499adba9b3d8740d67b3862ee
OGrocketa <jaroslaw.sedach@gmail.com>
2025-03-13 16:47:34 +0100

    wip

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
     <Routes>
-      <Route path="*" element={<Layout/>}>
+      <Route path="/*" element={<Layout/>}>
         <Route index element={<MainChatPage/>}/>
       </Route>
     </Routes>
